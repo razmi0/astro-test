25f78cf5c711e0d7254d6590af85bee23981d4b6
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-04-13 15:11:57 +0200

    Add ImageNames type definition to index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,6 +2,8 @@
 
 export type IconNames = "menu";
 
+export type ImageNames = `${"vijay-et-lou" | "lorem-lou" | "placeholder" | "lorem-vijay" | "oak"}.webp`;
+
 export type Prettyfy<T> = {
   [K in keyof T]: T[K];
 };
