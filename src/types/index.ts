94cf22e1437c4d6cfa6f41f2d541a163b4a0d8aa
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-04-16 19:27:51 +0200

    Moved types into @/types.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,36 +1,77 @@
 // Add your types here
 
+import type { Button } from "@/components/ui/Button";
 import type { LocalImageProps } from "astro/assets";
-import type { HTMLAttributes, HTMLTag } from "astro/types";
+import type { ComponentProps, HTMLAttributes, HTMLTag } from "astro/types";
+type IconNames = "menu";
 
-export type IconNames = "menu";
+type ImageNames = `${"vijay-et-lou" | "lorem-lou" | "placeholder" | "lorem-vijay" | "oak"}.webp`;
 
-export type ImageNames = `${"vijay-et-lou" | "lorem-lou" | "placeholder" | "lorem-vijay" | "oak"}.webp`;
-
-export type PropsWithContent<T extends HTMLTag> = {
+type PropsWithContent<T extends HTMLTag> = {
   content: string;
   props?: HTMLAttributes<T>;
 };
 
-export type Theme = "light" | "dark";
+type Theme = "light" | "dark";
 
-export type PropsNoContent<T extends HTMLTag> = {
+type PropsNoContent<T extends HTMLTag> = {
   props?: HTMLAttributes<T>;
 };
 
-export type ReactPropsWithContent<T> = {
+type ReactPropsWithContent<T> = {
   content: string;
   props?: T;
 };
 
-export type FadeImageProps = {
+type FadeImageProps = {
   props?: Omit<LocalImageProps<HTMLAttributes<"img">>, "src">;
   name: ImageNames;
 };
 
-export type HTMLHeadingTags = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
-export type ContentFormat = "card" | "text" | "image-text" | "text-image";
+type HTMLHeadingTags = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+type ContentFormat = "card" | "text" | "image-text" | "text-image";
 
-export type Prettyfy<T> = {
+interface SharedProps<T extends ContentFormat> {
+  type: T;
+  title?: PropsWithContent<HTMLHeadingTags>;
+  subtitle?: PropsWithContent<"small">;
+  text?: PropsWithContent<"p">;
+  article?: PropsNoContent<"article">;
+  theme?: Theme;
+}
+
+interface ImageProps {
+  img?: FadeImageProps;
+  figure?: PropsNoContent<"figure">;
+  figCaption?: PropsWithContent<"figcaption">;
+}
+
+interface FormProps {
+  inputProps?: PropsNoContent<"input">;
+  buttonProps?: ReactPropsWithContent<ComponentProps<typeof Button>>;
+  labelProps?: PropsWithContent<"label">;
+  formProps?: PropsNoContent<"form">;
+}
+
+type ContentPropsMap = {
+  "image-text": ImageProps & SharedProps<"image-text"> & FormProps;
+  "text-image": ImageProps & SharedProps<"text-image"> & FormProps;
+  card: SharedProps<"card"> & FormProps;
+  text: SharedProps<"text"> & FormProps;
+};
+
+type Prettyfy<T> = {
   [K in keyof T]: T[K];
+};
+
+export type {
+  ContentFormat,
+  ContentPropsMap,
+  FadeImageProps,
+  FormProps,
+  HTMLHeadingTags,
+  IconNames,
+  ImageNames,
+  Prettyfy,
+  Theme,
 };
