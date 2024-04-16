e5acddd35ae24f31c2a55d502df4f55d44f561ba
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-04-16 22:26:25 +0200

    Comments

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -17,6 +17,11 @@ export default {
       "2xs": "375px",
       xs: "475px",
       ...defaultTheme.screens,
+      // 'sm': '640px' => (min-width: 640px)
+      // 'md': '768px' => (min-width: 768px)
+      // 'lg': '1024px' => (min-width: 1024px)
+      // 'xl': '1280px' => (min-width: 1280px)
+      // '2xl': '1536px' => (min-width: 1536px)
     },
     extend: {
       colors: {
