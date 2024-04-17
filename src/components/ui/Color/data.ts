7fc361afac8f37830ae0c5a1c164b43b407f3d8e
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-04-17 16:36:47 +0200

    Add color theming to ScreenUnit components

---
+++ src/components/ui/Color/data.ts
@@ -0,0 +1,14 @@
+/**
+ * colors
+ */
+
+const colorThemes = {
+  light: {
+    container: "bg-slate-300 dark:bg-slate-800",
+  },
+  dark: {
+    container: "bg-slate-400 dark:bg-slate-900",
+  },
+};
+
+export default colorThemes;
