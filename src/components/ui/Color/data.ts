f105bf7c4a80e004d4f98f95b6e08dd5b70c153a
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-04-17 21:36:37 +0200

    Remove unused image & add new hero image

--- src/components/ui/Color/data.ts
+++ src/components/ui/Color/data.ts
@@ -5,11 +5,13 @@
 const colorThemes = {
   light: {
     container: "bg-slate-300 dark:bg-slate-800",
+    carpet: "before:bg-slate-400 dark:before:bg-slate-900",
     title: "text-main-500 dark:text-main-300",
     titleBorder: "border-s-main-500 dark:border-s-main-300",
   },
   dark: {
     container: "bg-slate-400 dark:bg-slate-900",
+    carpet: "before:bg-slate-300 dark:before:bg-slate-800",
     title: "text-main-300 dark:text-main-300",
     titleBorder: "border-s-main-300 dark:border-s-main-500",
   },
