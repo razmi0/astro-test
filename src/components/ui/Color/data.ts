a18b8937337743b624648af949889aec159d23ac
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-04-17 18:29:56 +0200

    Add theme hub for colors for component with special thems

--- src/components/ui/Color/data.ts
+++ src/components/ui/Color/data.ts
@@ -5,10 +5,13 @@
 const colorThemes = {
   light: {
     container: "bg-slate-300 dark:bg-slate-800",
+    title: "text-main-500 dark:text-main-300",
+    titleBorder: "border-s-main-500 dark:border-s-main-300",
   },
   dark: {
     container: "bg-slate-400 dark:bg-slate-900",
+    title: "text-main-300 dark:text-main-300",
+    titleBorder: "border-s-main-300 dark:border-s-main-500",
   },
 };
-
 export default colorThemes;
