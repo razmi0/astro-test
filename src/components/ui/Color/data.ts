a4ca72ae8b1a7595842041683683aa37be813987
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-04-18 12:17:02 +0200

    Change colorThemes title and add separator prop

--- src/components/ui/Color/data.ts
+++ src/components/ui/Color/data.ts
@@ -6,13 +6,15 @@ const colorThemes = {
   light: {
     container: "bg-defs-100 dark:bg-slate-800",
     carpet: "before:bg-slate-400 dark:before:bg-slate-900",
-    title: "text-main-500 dark:text-main-300",
-    titleBorder: "border-s-main-500 dark:border-s-main-300",
+    title: "text-main-300 dark:text-main-300",
+    separator: "bg-main-300 dark:bg-main-300",
+    titleBorder: "border-s-main-300 dark:border-s-main-300",
   },
   dark: {
     container: "bg-slate-400 dark:bg-slate-900",
     carpet: "before:bg-slate-300 dark:before:bg-slate-800",
     title: "text-main-300 dark:text-main-300",
+    separator: "bg-main-300 dark:bg-main-300",
     titleBorder: "border-s-main-300 dark:border-s-main-500",
   },
 };
