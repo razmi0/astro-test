24094c325c9676e11ee8239a302d7adeee06c0a9
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-04-20 00:05:28 +0200

    Refactor ChevronDown component in Accordion.tsx

--- src/components/ui/Accordion.tsx
+++ src/components/ui/Accordion.tsx
@@ -6,14 +6,7 @@ import { cn } from "@/lib/utils";
 const Accordion = AccordionPrimitive.Root;
 
 const ChevronDown = ({ className, ...props }: { className?: string }) => (
-  <svg
-    viewBox="0 0 512 512"
-    version="1.1"
-    xmlns="http://www.w3.org/2000/svg"
-    fill="currentcolor"
-    className={className}
-    {...props}
-  >
+  <svg viewBox="0 0 512 512" fill="currentcolor" className={className} {...props}>
     <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
       <g fill="currentcolor" transform="translate(32.000000, 42.666667)">
         <path d="M246.3129 5.6289C252.9276 9.4087 258.4096 14.8907 262.1894 21.5054L444.667 340.8413C456.3581 361.3007 449.25 387.3638 428.7906 399.0549 422.3438 402.7388 415.0471 404.6766 407.622 404.6766L42.6667 404.6766C19.1025 404.6766 0 385.574 0 362.0099 0 354.5847 1.9377 347.2881 5.6216 340.8413L188.0993 21.5054C199.7904 1.046 225.8535-6.0622 246.3129 5.6289Z"></path>
