c60667f52fefd3b2e54bc7a8fadf8b368203d036
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-04-20 12:23:45 +0200

    Add buttonText to colorThemes

--- src/components/ui/Color/data.ts
+++ src/components/ui/Color/data.ts
@@ -5,7 +5,7 @@
 // --
 const defaultThemeValue = "pur";
 
-type ColorThemedKey = "container" | "title" | "separator" | "titleBorder" | "cssVar" | "button";
+type ColorThemedKey = "container" | "title" | "separator" | "titleBorder" | "cssVar" | "button" | "buttonText";
 
 const colorThemes = {
   default: (type: ColorThemedKey) => {
@@ -18,6 +18,7 @@ const colorThemes = {
     titleBorder: `border-s-${defaultThemeValue}-600 dark:border-s-${defaultThemeValue}-400`,
     cssVar: `var(--color-${defaultThemeValue}-500)`,
     button: `bg-${defaultThemeValue}-600 dark:bg-${defaultThemeValue}-600`,
+    buttonText: `text-${defaultThemeValue}-600 dark:text-${defaultThemeValue}-600`,
   },
   dark: {
     container: `bg-slate-400 dark:bg-slate-900`,
@@ -26,6 +27,7 @@ const colorThemes = {
     titleBorder: `border-s-${defaultThemeValue}-400 dark:border-s-${defaultThemeValue}-400`,
     cssVar: `color-${defaultThemeValue}-500`,
     button: `bg-${defaultThemeValue}-400 dark:bg-${defaultThemeValue}-400`,
+    buttonText: `text-${defaultThemeValue}-400 dark:text-${defaultThemeValue}-400`,
   },
 };
 
