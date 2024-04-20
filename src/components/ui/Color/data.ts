f36d777fc887f305242278163446545a2334a882
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-04-20 11:49:38 +0200

    checkpoint : 20/04/2024:11:49:37,90

--- src/components/ui/Color/data.ts
+++ src/components/ui/Color/data.ts
@@ -5,7 +5,7 @@
 // --
 const defaultThemeValue = "pur";
 
-type ColorThemedKey = "container" | "title" | "separator" | "titleBorder" | "cssVar";
+type ColorThemedKey = "container" | "title" | "separator" | "titleBorder" | "cssVar" | "button";
 
 const colorThemes = {
   default: (type: ColorThemedKey) => {
@@ -13,17 +13,19 @@ const colorThemes = {
   },
   light: {
     container: `bg-def-100 dark:bg-slate-800`,
-    title: `text-${defaultThemeValue}-500 dark:text-${defaultThemeValue}-400`,
-    separator: `bg-${defaultThemeValue}-500 dark:bg-${defaultThemeValue}-400`,
-    titleBorder: `border-s-${defaultThemeValue}-500 dark:border-s-${defaultThemeValue}-400`,
+    title: `text-${defaultThemeValue}-600 dark:text-${defaultThemeValue}-600`,
+    separator: `bg-${defaultThemeValue}-600 dark:bg-${defaultThemeValue}-600`,
+    titleBorder: `border-s-${defaultThemeValue}-600 dark:border-s-${defaultThemeValue}-400`,
     cssVar: `var(--color-${defaultThemeValue}-500)`,
+    button: `bg-${defaultThemeValue}-600 dark:bg-${defaultThemeValue}-600`,
   },
   dark: {
     container: `bg-slate-400 dark:bg-slate-900`,
-    title: `text-${defaultThemeValue}-500 dark:text-${defaultThemeValue}-400`,
-    separator: `bg-${defaultThemeValue}-500 dark:bg-${defaultThemeValue}-400`,
-    titleBorder: `border-s-${defaultThemeValue}-500 dark:border-s-${defaultThemeValue}-400`,
+    title: `text-${defaultThemeValue}-400 dark:text-${defaultThemeValue}-400`,
+    separator: `bg-${defaultThemeValue}-400 dark:bg-${defaultThemeValue}-400`,
+    titleBorder: `border-s-${defaultThemeValue}-400 dark:border-s-${defaultThemeValue}-400`,
     cssVar: `color-${defaultThemeValue}-500`,
+    button: `bg-${defaultThemeValue}-400 dark:bg-${defaultThemeValue}-400`,
   },
 };
 
