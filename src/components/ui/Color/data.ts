b5877a68fd9c9f6d425224605f1e8c99eecac201
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-04-20 11:29:39 +0200

    checkpoint : 20/04/2024:11:29:39,24

--- src/components/ui/Color/data.ts
+++ src/components/ui/Color/data.ts
@@ -1,19 +1,30 @@
 /**
  * colors
  */
+// pur or gr
+// --
+const defaultThemeValue = "pur";
+
+type ColorThemedKey = "container" | "title" | "separator" | "titleBorder" | "cssVar";
 
 const colorThemes = {
+  default: (type: ColorThemedKey) => {
+    return colorThemes["light"][type];
+  },
   light: {
-    container: "bg-def-100 dark:bg-slate-800",
-    title: "text-gr-500 dark:text-gr-400",
-    separator: "bg-gr-500 dark:bg-gr-400",
-    titleBorder: "border-s-gr-500 dark:border-s-gr-400",
+    container: `bg-def-100 dark:bg-slate-800`,
+    title: `text-${defaultThemeValue}-500 dark:text-${defaultThemeValue}-400`,
+    separator: `bg-${defaultThemeValue}-500 dark:bg-${defaultThemeValue}-400`,
+    titleBorder: `border-s-${defaultThemeValue}-500 dark:border-s-${defaultThemeValue}-400`,
+    cssVar: `var(--color-${defaultThemeValue}-500)`,
   },
   dark: {
-    container: "bg-slate-400 dark:bg-slate-900",
-    title: "text-gr-500 dark:text-gr-400",
-    separator: "bg-gr-500 dark:bg-gr-400",
-    titleBorder: "border-s-gr-500 dark:border-s-gr-400",
+    container: `bg-slate-400 dark:bg-slate-900`,
+    title: `text-${defaultThemeValue}-500 dark:text-${defaultThemeValue}-400`,
+    separator: `bg-${defaultThemeValue}-500 dark:bg-${defaultThemeValue}-400`,
+    titleBorder: `border-s-${defaultThemeValue}-500 dark:border-s-${defaultThemeValue}-400`,
+    cssVar: `color-${defaultThemeValue}-500`,
   },
 };
+
 export default colorThemes;
