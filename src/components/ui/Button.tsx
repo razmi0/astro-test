cb4c217be90df0818467cf9a0503117bd1c42c50
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-04-20 12:23:34 +0200

    Update link variant in Button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -24,7 +24,7 @@ const buttonVariants = cva(base.join(" "), {
       secondary:
         "bg-slate-100 text-slate-900 hover:bg-slate-100/80 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-slate-800/80",
       ghost: "hover:border-def-900 dark:hover:border-def-100",
-      link: "text-slate-900 underline-offset-4 hover:underline dark:text-slate-50",
+      link: "text-slate-900 underline-offset-4 hover:underline dark:text-slate-50 text-md",
     },
     size: {
       default: "h-10 px-4 py-2",
