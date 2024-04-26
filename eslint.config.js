9ffbfbafb64acc3b763b618e08c24c0e60d0c8f2
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-04-27 01:05:17 +0200

    Update eslint.config.js to use strict rules for Astro plugin

--- eslint.config.js
+++ eslint.config.js
@@ -2,14 +2,14 @@ import eslintPluginAstro from "eslint-plugin-astro";
 export default [
   // add more generic rule sets here, such as:
   // js.configs.recommended,
-  ...eslintPluginAstro.configs["flat/recommended"],
+  ...eslintPluginAstro.configs["flat/strict"],
   eslintPluginAstro.configs["flat/typescript"],
   eslintPluginAstro.configs["flat/jsx-a11y-strict"],
 
   {
     rules: {
       // override/add rules settings here, such as:
-      "no-unused-vars": ["warn"],
+      "typescript/no-unused-vars": ["warn"],
       // "astro/no-set-html-directive": "error"
     },
   },
