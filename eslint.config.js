892a6972e573c1c7a29812a4c773d420cd9f4c3d
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-04-27 11:24:32 +0200

    FIX : eslint not loading recommended modules

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,58 @@
-import pluginJs from "@eslint/js";
+import js from "@eslint/js";
+import typescriptParser from "@typescript-eslint/parser";
+import astroEslintParser from "astro-eslint-parser";
 import eslintPluginAstro from "eslint-plugin-astro";
-import jsxA11y from "eslint-plugin-jsx-a11y-strict";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default [
-  pluginJs.configs.recommended,
+  js.configs.recommended,
+  ...eslintPluginAstro.configs["flat/recommended"],
   ...tseslint.configs.recommended,
-  ...eslintPluginAstro.configs.recommended,
-  ...jsxA11y.configs['jsx-a11y-strict'],
   {
-    languageOptions: { globals: globals.browser },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
   },
   {
-    rules: {
-      "no-unused-vars": "error",
-      "no-undef": "error",
+    files: ["**/*.astro"],
+    languageOptions: {
+      parser: astroEslintParser,
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
     },
+  },
+  {
+    files: ["**/*.{js,jsx,astro}"],
+    rules: {
+      "no-mixed-spaces-and-tabs": ["error", "smart-tabs"],
+    },
+  },
+  {
+    // Define the configuration for `<script>` tag.
+    // Script in `<script>` is assigned a virtual file name with the `.js` extension.
+    files: ["**/*.{ts,tsx}", "**/*.astro/*.js"],
+    languageOptions: {
+      parser: typescriptParser,
+    },
+    rules: {
+      // Note: you must disable the base rule as it can report incorrect errors
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+        },
+      ],
+      "@typescript-eslint/no-non-null-assertion": "off",
+    },
+  },
+  {
+    ignores: ["dist", "node_modules", ".github", "types.generated.d.ts"],
   },
 ];
